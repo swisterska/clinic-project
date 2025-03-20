c2a2eb45611ae1668babd2a628378a0e930169ae
Julia Swisterska <278448@student.pwr.edu.pl>
2025-03-20 10:42:18 +0100

    logreg

---
+++ app/src/main/java/com/example/eclinic/LogRegActivity.kt
@@ -0,0 +1,39 @@
+package com.example.eclinic
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * LogRegActivity is the activity where the user chooses to either log in, register, or go back to the main page.
+ * It handles the navigation between different screens like the LoginActivity, RegisterActivity, and MainPageActivity.
+ */
+class LogRegActivity : AppCompatActivity() {
+
+    /**
+     * This method is called when the activity is created.
+     * It sets up the UI elements and defines click listeners for buttons to navigate to other activities.
+     *
+     * @param savedInstanceState a Bundle object that contains the activity's previously saved state, or null if there is no state.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.log_reg_activity)
+
+        // Set up the "Login" button to navigate to the LoginActivity when clicked
+        val buttonLogin = findViewById<ImageButton>(R.id.ChoiceLoginButton)
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Set up the "Register" button to navigate to the RegisterActivity when clicked
+        val buttonRegister = findViewById<ImageButton>(R.id.ChoiceRegisterButton)
+        buttonRegister.setOnClickListener {
+
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/eclinic/RegisterActivity.kt
+++ app/src/main/java/com/example/eclinic/RegisterActivity.kt
@@ -6,11 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoadingActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.loading_activity)
+        setContentView(R.layout.activity_register2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
