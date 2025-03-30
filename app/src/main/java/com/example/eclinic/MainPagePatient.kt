0509508720fef75a99d87777e8a27dbd79290f75
Julia Swisterska <278448@student.pwr.edu.pl>
2025-03-30 15:28:27 +0200

    Main page

--- app/src/main/java/com/example/eclinic/MainPagePatient.kt
+++ app/src/main/java/com/example/eclinic/MainPagePatient.kt
@@ -1,16 +1,72 @@
 package com.example.eclinic
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.TextView
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainPagePatient : AppCompatActivity() {
+
+    private lateinit var welcomeText: TextView
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main_page_patient)
 
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        welcomeText = findViewById(R.id.tvWelcomeMessage)
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            firestore.collection("users").document(userId).get()
+                .addOnSuccessListener { document ->
+                    if (document != null && document.exists()) {
+                        val firstName = document.getString("firstName") ?: "User"
+                        welcomeText.text = "Welcome, dr. $firstName"
+                    }
+                }
+                .addOnFailureListener {
+                    welcomeText.text = "Welcome, User"
+                }
+
+        }
+
+
+
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_chat -> {
+                    startActivity(Intent(this, ChatPatientActivity::class.java))
+                    true
+                }
+                R.id.nav_prescriptions -> {
+                    startActivity(Intent(this, PrescriptionsActivity::class.java))
+                    true
+                }
+                R.id.nav_appointments -> {
+                    startActivity(Intent(this, AppointmentsActivity::class.java))
+                    true
+                }
+                R.id.nav_profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
 }
